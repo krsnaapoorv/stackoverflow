f58517029eec7d8d6a643d744658ed2cf057d71b
krsnaapoorv <apoorva15997@gmail.com>
2020-05-03 17:36:53 +0530

    update

---
+++ src/components/cards.js
@@ -0,0 +1,27 @@
+import React from 'react'
+const Cards = (props)=>{
+    return(
+        <React.Fragment>
+            <div className="card m-2" style = {{width:"18rem"}}>
+                <div className="card-body">
+                    <h5 className="card-title">{props.label.title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted"> Question id :-  {props.label.question_id}</h6>
+                    <h6>By <a href={props.label.owner.link} className="card-link">{props.label.owner.display_name}</a></h6>
+                    <a href={props.label.link} className="card-link" target="blank">Stackoverflow Link</a>
+                    <div>
+                            <p>Toal View Count:  {props.label.view_count}</p>
+                            <p>Raised at:   {Date(props.label.creation_date)}</p>
+                            <h6>Tags</h6>
+                            <div className="row justify-content-center">
+                                {props.label.tags.map(ele =>
+                                    <span  className="badge badge-pill bg-primary m-2">{ele}</span>
+                                )}
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>          
+    )
+}
+
+export default Cards
--- src/components/stack.js
+++ src/components/stack.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+import Card from './cards'
 
 class  Stack extends React.Component {
     constructor(props){
@@ -7,7 +9,7 @@ class  Stack extends React.Component {
             tagname : "",
             quesId : "",
             data : [],
-            table : true
+            output : true
         }
     }
 
@@ -17,10 +19,30 @@ class  Stack extends React.Component {
         });
     }
 
+    getTop = () =>{
+        let tag = this.state.tagname
+        let baseUrl = "https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&order=desc&sort=creation&tagged="
+        let url = baseUrl+tag+"&filter=default&site=stackoverflow"
+        axios.get(url)
+        .then(res => {
+            const data = res.data.items
+            console.log(data)
+            this.setState({
+                data : data,
+                output : true
+            })
+        })
+        .catch(error => alert(error))
+        this.setState({
+            tagname : ""
+        })
+
+    }
+
     render(){
         return (
-            <div>
-                <div className="d-flex justify-content-center">
+            <div className="container-fluid">
+                <div className="row justify-content-center">
                     <img src="/stackoverflow.png" alt="Stackoverflow logo" height="80px" ></img>
                 </div>
                 <h1 className = "display-3 text-center font-weight-normal">Stack Exchange</h1>
@@ -36,12 +58,26 @@ class  Stack extends React.Component {
                     <div className = "col">
                         <div className = "col">
                             <div className="form-group w-50 ml-3">
-                                <label htmlFor="exampleInputEmail1" className="h3 font-weight-bold">Search for Question Id</label>
+                                <label htmlFor="exampleInputEmail1" className="h3 font-weight-bold">Search for Question(Based on Id)</label>
                                 <input onChange={this.handleChange} name = "quesId" value ={this.state.quesId} type="text" className="form-control border-danger"  placeholder="Enter question id" />
                             </div>
                             <button   type="submit" onClick={this.getQuesAns} className="btn btn-primary ml-3">Submit</button>
                         </div>
                     </div>
+                </div>
+                <div className="mt-5">
+                    <h2 className="text-center">Questions/Answers</h2>
+                    {this.state.output ? ( 
+                        <div className="container-fluid row justify-content-center">
+                            {this.state.data.map(ele=> 
+                                <Card label={ele}/>
+                            )}
+                        </div>
+                    ):(
+                        <div className="container-fluid row justify-content-center">
+                            
+                        </div>
+                    )}
                 </div>
             </div>
         )
