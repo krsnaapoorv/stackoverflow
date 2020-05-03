33ba52ac2e629469cfe8e9af2e157c06a08e7d35
krsnaapoorv <apoorva15997@gmail.com>
2020-05-03 20:58:16 +0530

    complete

---
+++ src/components/answers.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactHtmlParser from 'react-html-parser'
+
+class Answers extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data : []
+        }
+    }
+
+    componentDidMount = () =>{
+        let arr = this.props.label.filter(ele => ele.is_accepted === "true")
+        if(arr.length === 0){
+            // console.log(this.props.label)
+            this.setState({
+                data : this.props.label
+            })
+        }
+        else{
+            this.setState({
+                data :arr
+            })
+        }
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                {this.state.data.map(ele => 
+                    <div className="card m-2">
+                        <div className="card-body">
+                            <h5 className="card-title">{ele.answer_id}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">Answer Acception rate of User :-  {ele.owner.accept_rate}%</h6>
+                            <h6>By <a href={ele.owner.link} className="card-link">{ele.owner.display_name}</a></h6>
+                            <div>
+                                <p>Is Accepted? : {ele.is_accepted.toString()}</p>
+                                <p>Answered at:   {Date(ele.last_activity_date)}</p>
+                            </div>
+                            <h4>Answer:-</h4>
+                            <div>
+                                {ReactHtmlParser(ele.body)}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </React.Fragment>          
+        )
+    }
+}
+
+export default Answers
--- src/components/stack.js
+++ src/components/stack.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import Card from './cards'
+import Answers from './answers'
 
 class  Stack extends React.Component {
     constructor(props){
@@ -9,7 +10,9 @@ class  Stack extends React.Component {
             tagname : "",
             quesId : "",
             data : [],
-            output : true
+            answer : [],
+            output : false,
+            noAns : false
         }
     }
 
@@ -26,7 +29,7 @@ class  Stack extends React.Component {
         axios.get(url)
         .then(res => {
             const data = res.data.items
-            console.log(data)
+            // console.log(data)
             this.setState({
                 data : data,
                 output : true
@@ -39,6 +42,49 @@ class  Stack extends React.Component {
 
     }
 
+    ques = async() => {
+        let quesId = this.state.quesId
+        let url = `https://api.stackexchange.com/2.2/questions/${quesId}?order=desc&sort=activity&site=stackoverflow`
+        axios.get(url)
+        .then(res => {
+            const data = res.data.items
+            // console.log(data)
+            this.setState({
+                data : data,
+                output : false
+            })
+        })
+        .catch(error => alert(error))
+
+        let ansUrl = `https://api.stackexchange.com/2.2/questions/${quesId}/answers?order=desc&sort=votes&site=stackoverflow&filter=withbody`
+        await axios.get(ansUrl)
+        .then(res => {
+            const data = res.data.items
+            if(data.length === 0){
+                this.setState({
+                    output : false,
+                    noAns : false
+                })
+            }
+            else{
+                this.setState({
+                    answer : data,
+                    output : false,
+                    noAns : true
+                })
+            }
+        })
+        .catch(error => alert(error))
+        this.setState({
+            quesId : ""
+        })
+        console.log(this.state.answer)
+    }
+
+    getQuesAns = () => {
+        this.ques()
+    }
+
     render(){
         return (
             <div className="container-fluid">
@@ -48,21 +94,19 @@ class  Stack extends React.Component {
                 <h1 className = "display-3 text-center font-weight-normal">Stack Exchange</h1>
                 <h5 className="text-center">Search by Tag name or Question Id</h5>
                 <div className="row mt-4">
-                    <div className = "col">
-                        <div className="form-group ml-5">
+                    <div className = "col-md-6 row-sm">
+                        <div className="form-group ml-md-5 ">
                             <label htmlFor="exampleInputEmail1" className="h3 font-weight-bold">Search for Tag(top 10)</label>
                             <input onChange={this.handleChange} name = "tagname" value ={this.state.tagname} type="text" className="form-control border-danger w-50"  placeholder="Enter tagname" />
                         </div>
                         <button   type="submit" onClick={this.getTop} className="btn btn-primary ml-5">Submit</button>
                     </div>
-                    <div className = "col">
-                        <div className = "col">
-                            <div className="form-group w-50 ml-3">
-                                <label htmlFor="exampleInputEmail1" className="h3 font-weight-bold">Search for Question(Based on Id)</label>
-                                <input onChange={this.handleChange} name = "quesId" value ={this.state.quesId} type="text" className="form-control border-danger"  placeholder="Enter question id" />
-                            </div>
-                            <button   type="submit" onClick={this.getQuesAns} className="btn btn-primary ml-3">Submit</button>
+                    <div className = "col-md-6">
+                        <div className="form-group w-75 ml-md-5">
+                            <label htmlFor="exampleInputEmail1" className="h3 font-weight-bold">Search for Question(Based on Id)</label>
+                            <input onChange={this.handleChange} name = "quesId" value ={this.state.quesId} type="text" className="form-control border-danger"  placeholder="Enter question id" />
                         </div>
+                        <button   type="submit" onClick={this.getQuesAns} className="btn btn-primary ml-5">Submit</button>
                     </div>
                 </div>
                 <div className="mt-5">
@@ -75,7 +119,20 @@ class  Stack extends React.Component {
                         </div>
                     ):(
                         <div className="container-fluid row justify-content-center">
-                            
+                            <div className="container-fluid row justify-content-center">
+                                {this.state.data.map(ele=> 
+                                    <Card label={ele}/>
+                                )}
+                            </div>
+                            <div className="mt-5">
+                                <h5 className="text-center">Answers of above question</h5>
+                                {this.state.noAns ? (
+                                    <Answers label={this.state.answer}/>
+                                ):(
+                                    <h3>No Answers Available</h3>
+                                )}
+                                
+                            </div>
                         </div>
                     )}
                 </div>
